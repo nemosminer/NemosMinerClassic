de6896f11f6f0e2a74a75bd35ff73c0b5fecaf47
nemo <slbaker@hotmail.co.nz>
2018-11-21 12:36:18 +1300

    Add files via upload

---
+++ algorithm_update.bat
@@ -0,0 +1,7 @@
+curl -LJO https://github.com/nemosminer/NemosMinerClassic/InfoLookup/raw/master/Algorithms.7z
+
+7z x -y Algorithms.7z
+
+del Algorithms.7z
+
+exit
---
+++ miner_update.bat
@@ -0,0 +1,7 @@
+curl -LJO https://github.com/nemosminer/NemosMinerClassic/InfoLookup/raw/master/Miners.7z
+
+7z x -y Miners.7z
+
+del Miners.7z
+
+exit
