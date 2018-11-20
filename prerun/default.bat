5e2383efdae3b65fd3e34cb62028d8d2d35cf6ef
nemo <slbaker@hotmail.co.nz>
2018-11-21 12:38:00 +1300

    Create default.bat

---
+++ prerun/default.bat
@@ -0,0 +1,7 @@
+REM This file will run for any algo if AlgoName.bat does not exist
+REM Place your code below and rename this file to default.bat
+
+echo prerun default file
+
+REM Example clock settings using nvidiaInspector update nvidiaInspector.exe path accordingly or place it in prerun directory
+REM !!! USE OC WITH CAUTION !!!
